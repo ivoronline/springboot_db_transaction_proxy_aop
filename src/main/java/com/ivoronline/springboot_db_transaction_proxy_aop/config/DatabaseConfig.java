e2178c2b283c84079ef579f545c93dcb6dbcc879
ivoronline <ivoronline@gmail.com>
2024-07-09 10:38:03 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_transaction_proxy_aop/config/DatabaseConfig.java
+++ src/main/java/com/ivoronline/springboot_db_transaction_proxy_aop/config/DatabaseConfig.java
@@ -2,7 +2,6 @@ package com.ivoronline.springboot_db_transaction_proxy_aop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import javax.sql.DataSource;
@@ -10,9 +9,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @Configuration
-@ComponentScan("com")
 @EnableAspectJAutoProxy
-
 public class DatabaseConfig {
 
   //PROPERTIES
